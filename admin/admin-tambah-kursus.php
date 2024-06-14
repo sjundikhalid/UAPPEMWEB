f7f1fff0edcdd1a55620fb8d43f65f522d176e05
Suntan Jundi Khalid <112401444+sjundikhalid@users.noreply.github.com>
2024-06-14 08:38:40 +0700

    Update admin-tambah-kursus.php

--- admin/admin-tambah-kursus.php
+++ admin/admin-tambah-kursus.php
@@ -1,5 +1,10 @@
 <?php
+session_start();
 include '../conn.php';
+if (!isset($_SESSION['email']) || $_SESSION['roles'] !== 'admin') {
+    header('Location: ../login.php');
+    exit();
+}
 
 if (isset($_POST['tambah'])) {
     $namaCourse = htmlspecialchars($_POST['namaCourse']);
