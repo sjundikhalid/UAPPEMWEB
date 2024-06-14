5c557a9f24755250043529d6ef0e23d107e8129b
liraseptiyani <112398818+liraseptiyani@users.noreply.github.com>
2024-06-14 09:05:18 +0700

    Update admin-hapus-mentor.php

--- admin/admin-hapus-mentor.php
+++ admin/admin-hapus-mentor.php
@@ -14,17 +14,16 @@ if (!isset($_GET['id'])) {
   exit();
 }
 $id = $_GET['id'];
-$query = "DELETE FROM users WHERE email='$id'";
-mysqli_query($conn, $query);
+$query = "DELETE FROM materi WHERE idMateri='$id'";
 if (mysqli_query($conn, $query)) {
     echo "<script>
-            alert('Data Mentor Berhasil Dihapus');
-            document.location.href = 'admin-kelola-mentor.php';
+            alert('Data Materi Berhasil Dihapus');
+            document.location.href = 'admin-daftar-materi.php';
           </script>";
 } else {
     echo "<script>
-            alert('Data Mentor Gagal Dihapus');
-            document.location.href = 'admin-kelola-mentor.php';
+            alert('Data Materi Gagal Dihapus');
+            document.location.href = 'admin-daftar-materi.php';
           </script>";
 }
 ?>
