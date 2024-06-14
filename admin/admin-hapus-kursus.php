93b56c7ab24e9598a614489ab8c06184375b4bac
Suntan Jundi Khalid <112401444+sjundikhalid@users.noreply.github.com>
2024-06-14 08:35:43 +0700

    Update admin-hapus-kursus.php

--- admin/admin-hapus-kursus.php
+++ admin/admin-hapus-kursus.php
@@ -1,5 +1,18 @@
 <?php
+session_start();
 include '../conn.php';
+if (!isset($_SESSION['email']) || $_SESSION['roles'] !== 'admin') {
+    header('Location: ../login.php');
+    exit();
+}
+
+if (!isset($_GET['id'])) {
+  echo "<script>
+          alert('Tidak ada ID yang dipilih');
+          document.location.href = 'admin-kelola-mentor.php';
+        </script>";
+  exit();
+}
 $id = $_GET['id'];
 $query = "DELETE FROM course WHERE idCourse='$id'";
 if (mysqli_query($conn, $query)) {
